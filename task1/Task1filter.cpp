051b9973cf1e447f81b6465a8f0dd165481a19fe
Hoonie <s3774430@student.rmit.edu.au>
2022-08-08 09:49:28 +1000

    The bash script and the cpp file produce the same output

--- task1/Task1filter.cpp
+++ task1/Task1filter.cpp
@@ -1,8 +1,9 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <regex>
-#include <memory>
+#include <string>
+#include <algorithm>
+
 //
 // Created by Myeonghoon Sun on 6/8/2022.
 //
@@ -14,19 +15,34 @@ void Task1Filter(const std::string &readFileName, const std::string &writeFileNa
     std::string line;
     std::vector<std::string> wordVec;
     std::ifstream input(readFileName);
+    std::ofstream output(writeFileName);
+    
     while (std::getline(input, line)) {
-        if (line.size() >= MIN_WORD_LENGTH &&
-            line.size() <= MAX_WORD_LENGTH &&
-            std::regex_match(line, std::regex("^[a-zA-Z]+$"))) {
-            wordVec.push_back(line);
+        if (line.size() >= MIN_WORD_LENGTH && line.size() <= MAX_WORD_LENGTH) {
+            // Only insert words that contain English lower characters
+            bool isAllLower = true;
+            for (char ch: line) {
+                if (!islower(ch)) {
+                    isAllLower = false;
+                }
+            }
+            if (isAllLower) {
+                wordVec.push_back(line);
+            }
         }
     }
+
+    std::stable_sort(wordVec.begin(), wordVec.end(),
+		    [](const std::string &a, const std::string &b) {
+			return a.substr(MIN_WORD_LENGTH - 1) < b.substr(MIN_WORD_LENGTH - 1);
+		    }); 
+
     for (auto word : wordVec) {
-        std::cout << word << std::endl;
+	    output << word << std::endl;
     }
 }
 
-int main(int argc, char *argv[]) {
-    Task1Filter("../input.txt", "shit");
-}
 
+int main(int argc, char *argv[]) {
+    Task1Filter("../input.txt", "output-cpp.txt");
+}
